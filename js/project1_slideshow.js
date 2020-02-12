75776cb935519d405888b37625627e8075a5bc37
Pedro Krieckaert <58333196+PEEKrieckaert@users.noreply.github.com>
2020-02-12 15:14:24 +0100

    new js folder + slideshow script, fixed missing video links

---
+++ js/project1_slideshow.js
@@ -0,0 +1,17 @@
+var pictureIndex = 1;
+displayImage(pictureIndex);
+
+function plusDivs(n) {
+  displayImage(pictureIndex += n);
+}
+
+function displayImage(n) {
+  var i;
+  var x = document.getElementsByClassName("Slideshow1");
+  if (n < 1) {pictureIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  if (n > x.length) {pictureIndex = 1}
+  x[pictureIndex-1].style.display = "block";
+  }
